d26a2c47d32a9e5c4f902a89ea94fc61c9f221cb
tomasagata <tomasagata00@gmail.com>
2021-11-13 16:03:27 -0300

    Agregada generación de vehículos por literales

--- modulos/GeneradorVehiculos.js
+++ modulos/GeneradorVehiculos.js
@@ -3,9 +3,54 @@ const Camion = require("./Camion");
 const Moto = require("./Moto");
 
 const generadorVehiculos = (() => {
+    var tipoVehiculos = {
+        "Auto": Auto,
+        "Camion": Camion,
+        "Moto": Moto
+    };
+
     const getRandomInt = () => {
         return Math.floor(Math.random() * 3); // Genera un entero aleatorio -> restringido a 0, 1 y 2
     }
+
+    const generarVehiculo = (o) => {
+
+        if (o.hasOwnProperty("tipoVehiculo") && tipoVehiculos[o.tipoVehiculo] != undefined) {
+
+            if (o.hasOwnProperty("capacidad") && o.capacidad > 0) {
+
+                if (o.hasOwnProperty("cantCombustible") && o.cantCombustible <= o.capacidad && o.cantCombustible >= 0) {
+
+                    return tipoVehiculos[o.tipoVehiculo](o.capacidad, o.cantCombustible);
+
+                } else if (!(o.hasOwnProperty("cantCombustible"))) {
+                    return tipoVehiculos[o.tipoVehiculo](o.capacidad);
+                }
+
+            } else if (!(o.hasOwnProperty("capacidad"))) {
+                return tipoVehiculos[o.tipoVehiculo]();
+            }
+
+            throw new Error("[x] Datos inválidos al crear Vehiculo");
+
+        }
+
+        return generarVehiculoRandom();
+
+    };
+
+    const generarVehiculos = (arrObj = []) => {
+        var arr = [];
+        arrObj.forEach((elem) => {
+            try {
+                arr.push(generarVehiculo(elem));
+            } catch (e) {
+                console.log(e + ": Eliminando...");
+            }
+        });
+
+        return arr;
+    };
 
     const generarVehiculoRandom = () => {
         let randomIntIndex = getRandomInt();
@@ -19,7 +64,7 @@ const generadorVehiculos = (() => {
         }
     }
 
-    const generarVehiculosRandom = cant => {
+    const generarVehiculosRandom = (cant = 0) => {
         let arr = [];
         for (let i = 0; i < cant; i++) {
             arr.push(generarVehiculoRandom());
@@ -29,7 +74,9 @@ const generadorVehiculos = (() => {
 
     return {
         generarVehiculoRandom,
-        generarVehiculosRandom
+        generarVehiculosRandom,
+        generarVehiculo,
+        generarVehiculos
     }
 })();
 
